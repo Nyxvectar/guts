eac680b5079aecc03fc9eca8ca10548318c73e9e
Nyxvectar Yan <Nyxvectar@proton.me>
2025-07-24 10:21:59 +0800

    Issues#6：增加对数换底公式

--- algebran/logexp.go
+++ algebran/logexp.go
@@ -4,7 +4,6 @@
  * Created: 07/23/2025
  */
 
-//"functions": [
 //"指对互换式",
 //"对数运算法则",
 //"对数换底公式",
@@ -12,3 +11,24 @@
 //"平均增长率公式"
 
 package algebran
+
+import (
+	"errors"
+	"math"
+)
+
+func LogBaseConvert(base, x float64) (float64, error) {
+	if base <= 0 || base == 1 {
+		var errNew = "对数的底须大于0且不为1"
+		return 0, errors.New(errNew)
+	} else if x <= 0 {
+		var errNew = "对数的指数须大于0"
+		return 0, errors.New(errNew)
+	} else {
+		var (
+			lnBase = math.Log(base)
+			lnX    = math.Log(x)
+		)
+		return lnBase / lnX, nil
+	}
+}
