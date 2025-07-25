04055d020384673a88cfeeb8befee8381472b74d
Nyxvectar Yan <Nyxvectar@proton.me>
2025-07-25 15:51:21 +0800

    refs #11 将多余的柱锥台公式收敛至两个

--- geometryn/solid.go
+++ geometryn/solid.go
@@ -20,14 +20,20 @@ var (
 )
 
 func checker(a, b float64) bool {
-	if a <= 0 || b <= 0 {
+	if a < 0 || b < 0 {
 		return false
 	} else {
 		return true
 	}
 }
 
-func AreaCylinder(r, h float64) (float64, error) {
+// 注意到台体的公式事实上是通用于圆柱体和圆锥的
+// 即后二者的公式只是台体公式的特殊情况，那么我
+// 们就可以只使用台体公式来同时解决此三者的问题
+// 对于圆锥，认为她的其中一个地面半径和面积都为
+// 零即可；对于圆柱体，认为其上下面是相同的即可
+
+func AreaCycle(r, h float64) (float64, error) {
 	if checker(r, h) {
 		return 2 * math.Pi * r * (r + h), nil
 	} else {
@@ -35,39 +41,7 @@ func AreaCylinder(r, h float64) (float64, error) {
 	}
 }
 
-func AreaCone(r, l float64) (float64, error) {
-	if checker(r, l) {
-		return math.Pi * r * (r + l), nil
-	} else {
-		return 0, errors.New(dntExist)
-	}
-}
-
-func AreaFlatTopCone(r1, r2, l float64) (float64, error) {
-	if checker(r1, l) && r2 > 0 {
-		return math.Pi * (r1*r1 + r2*r2 + (r1+r2)*l), nil
-	} else {
-		return 0, errors.New(dntExist)
-	}
-}
-
-func VolumeCylinder(s, h float64) (float64, error) {
-	if checker(s, h) {
-		return s * h, nil
-	} else {
-		return 0, errors.New(dntExist)
-	}
-}
-
-func VolumeCone(s, h float64) (float64, error) {
-	if checker(s, h) {
-		return s * h / 3, nil
-	} else {
-		return 0, errors.New(dntExist)
-	}
-}
-
-func VolumeFlatTopCone(s1, s2, h float64) (float64, error) {
+func VolumeCycle(s1, s2, h float64) (float64, error) {
 	if checker(s1, h) && s2 > 0 {
 		return (s1 + s2 + math.Sqrt(s1*s2)) * h / 3, nil
 	} else {
