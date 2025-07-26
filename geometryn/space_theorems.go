408847e1ccb19176483218c5aeb42564e2dff7c0
Nyxvectar Yan <Nyxvectar@proton.me>
2025-07-26 14:35:34 +0800

    refs #13 实现向量的加法与减法

--- geometryn/space_theorems.go
+++ geometryn/space_theorems.go
@@ -11,3 +11,15 @@ type SpatialCoordinateSys struct {
 	y float64
 	z float64
 }
+
+func NewSpatialPoint(x, y, z float64) SpatialCoordinateSys {
+	return SpatialCoordinateSys{x, y, z}
+}
+
+func (v SpatialCoordinateSys) Add(u SpatialCoordinateSys) SpatialCoordinateSys {
+	return SpatialCoordinateSys{v.x + u.x, v.y + u.y, v.z + u.z}
+}
+
+func (v SpatialCoordinateSys) Subtract(u SpatialCoordinateSys) SpatialCoordinateSys {
+	return SpatialCoordinateSys{v.x - u.x, v.y - u.y, v.z - u.z}
+}
